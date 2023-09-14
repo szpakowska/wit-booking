3b958a2b41822b21c25af70f50265b504ebb9405
Anna Książek <aksiazek@softak.eu>
2023-09-14 18:03:25 +0200

    add log config and N+1 example

--- src/test/java/pl/sdacademy/booking/util/DatabaseUtilIT.java
+++ src/test/java/pl/sdacademy/booking/util/DatabaseUtilIT.java
@@ -36,4 +36,23 @@ class DatabaseUtilIT {
         assertThat(result.get(0).getAttributes()).hasSize(1);
         assertThat(result.get(1).getPrice()).isEqualTo(BigDecimal.valueOf(450));
     }
+
+    @Test
+    void shouldFindAllItemsInCatalog() {
+
+        var query = entityManager.createQuery("select item from ItemEntity item "
+                + " order by item.id", ItemEntity.class);
+
+        var result = query.getResultList();
+        // jezeli z jakiegos powodu entity manages zostanie "w miedzyczasie" zamkniety pojawia sie problemy
+//        entityManager.close();
+
+        assertThat(result).hasSize(5);
+        // zadziala tylko dlatego, ze jest order by w zapytaniu
+        // jakby order nie byl dodany kazde wywolanie mogloby sie skonczyc inna kolejnoscia zwracanych danych
+        assertThat(result.get(0).getAttributes()).hasSize(1);
+        assertThat(result.get(1).getAttributes()).hasSize(1);
+        assertThat(result.get(2).getAttributes()).hasSize(1);
+        assertThat(result.get(2).getPrice()).isEqualTo(BigDecimal.valueOf(450));
+    }
 }
