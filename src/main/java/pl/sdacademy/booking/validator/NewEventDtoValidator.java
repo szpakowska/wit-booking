b1a45965242aa21572f2ad0b7ea2605a91f554e0
Anna Margery <anna.szewc@gmail.com>
2023-09-20 22:24:10 +0200

    validator: Added past date case

--- src/main/java/pl/sdacademy/booking/validator/NewEventDtoValidator.java
+++ src/main/java/pl/sdacademy/booking/validator/NewEventDtoValidator.java
@@ -1,8 +1,10 @@
 package pl.sdacademy.booking.validator;
 
 import pl.sdacademy.booking.model.NewEventDto;
-
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,10 +19,8 @@ public class NewEventDtoValidator {
             result.add("To is null");
         }
 
-        //date in the future
-        //date from 8 to 16
         //how long is event
-        if(newEventDto.getFromTime() != null && newEventDto.getToTime() != null) {
+        if (newEventDto.getFromTime() != null && newEventDto.getToTime() != null) {
             Duration duration = Duration.between(newEventDto.getFromTime(), newEventDto.getToTime());
             if (duration.isNegative()) {
                 result.add("To is before from");
@@ -29,7 +29,47 @@ public class NewEventDtoValidator {
                 result.add("Too long event");
             }
         }
+
+        //date in the future
+        //czy Local Date Time bierze pod uwage zmiany czasu?
+        if (newEventDto.getFromTime() != null) {
+            if (newEventDto.getFromTime().isBefore((LocalDateTime.now()))) {
+                result.add("From date is from the past");
+            }
+        }
+
+        if (newEventDto.getToTime() != null) {
+            if (newEventDto.getToTime().isBefore(LocalDateTime.now())) {
+                result.add("To date is from the past");
+            }
+        }
+
+        //date from 8 to 16
+
+        //dopuszczamy from wieksze lub rowne 8:00 i mniejsze niz 15:30
+        //dopuszczamy to wieksze od 8:30 i to najpozniej o 16:00
+
+        LocalTime open = LocalTime.of(8, 0);
+        LocalTime close = LocalTime.of(16, 0);
+
+        LocalDateTime.now(ZoneId.of("Europe/Warsaw"));
+
+
+//        int hour = newEventDto.getFromTime().getHour();
+//        int minutes = newEventDto.getFromTime().getMinute();
+
+
+        // godziny otewarcia salonu od osmej czyli mozna sprobowac doubla i zrobic cast hour na doubla.
+
+
         //item name is null
+        // NewEventDto jest null.
         return result;
     }
+
+//    public static boolean validateNullTimeValues(NewEventDto newEventDto) {
+//        boolean timeValuesAreNulls;
+//
+//
+//    }
 }
--- src/test/java/pl/sdacademy/booking/validator/NewEventDtoValidatorTest.java
+++ src/test/java/pl/sdacademy/booking/validator/NewEventDtoValidatorTest.java
@@ -2,7 +2,6 @@ package pl.sdacademy.booking.validator;
 
 import org.junit.jupiter.api.Test;
 import pl.sdacademy.booking.model.NewEventDto;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,7 +15,7 @@ class NewEventDtoValidatorTest {
                 .toTime(LocalDateTime.of(2023,9,19,19,56))
                 .build();
         List<String> result=NewEventDtoValidator.validate(input);
-        assertThat(result).hasSize(1).contains("From is null");
+        assertThat(result).hasSize(2).contains("From is null");
     }
     @Test
     void shouldCheckThatToIsNull() {
@@ -25,7 +24,7 @@ class NewEventDtoValidatorTest {
                 .fromTime(LocalDateTime.of(2023,9,19,19,56))
                 .build();
         List<String> result=NewEventDtoValidator.validate(input);
-        assertThat(result).hasSize(1).contains("To is null");
+        assertThat(result).hasSize(2).contains("To is null");
     }
     @Test
     void shouldCheckThatToAndFromIsNull() {
@@ -36,4 +35,34 @@ class NewEventDtoValidatorTest {
         List<String> result=NewEventDtoValidator.validate(input);
         assertThat(result).hasSize(2).contains("To is null");
     }
+
+    @Test
+    void shouldCheckThatFromIsPast(){
+        NewEventDto input= NewEventDto.builder().itemName("przyklad")
+                .toTime(LocalDateTime.now().plusSeconds(1L))
+                .fromTime(LocalDateTime.now().minusSeconds(1L))
+                .build();
+        List<String> result=NewEventDtoValidator.validate(input);
+        assertThat(result).hasSize(1).contains("From date is from the past");
+    }
+    @Test
+    void shouldCheckThatToIsPast(){
+        NewEventDto input= NewEventDto.builder().itemName("przyklad")
+                .fromTime(LocalDateTime.now().plusSeconds(1L))
+                .toTime(LocalDateTime.now().minusSeconds(1L))
+                .build();
+        List<String> result=NewEventDtoValidator.validate(input);
+        assertThat(result).hasSize(2).contains("To date is from the past");
+    }
+    @Test
+    void shouldCheckThatToAndFromIsPast(){
+        NewEventDto input= NewEventDto.builder().itemName("przyklad")
+                .fromTime(LocalDateTime.now().minusSeconds(1L))
+                .toTime(LocalDateTime.now().minusSeconds(1L))
+                .build();
+        List<String> result=NewEventDtoValidator.validate(input);
+        assertThat(result).hasSize(2).contains("To date is from the past");
+    }
+
+
 }
