f4365eff0f0105280052f120209fdb3aa86b4853
Anna Margery <anna.szewc@gmail.com>
2023-09-21 23:51:54 +0200

    validator: Started refactoring validate method (not finished) test: added test for maximum and minimum session duration

--- src/main/java/pl/sdacademy/booking/validator/NewEventDtoValidator.java
+++ src/main/java/pl/sdacademy/booking/validator/NewEventDtoValidator.java
@@ -10,21 +10,22 @@ public class NewEventDtoValidator {
 
     private static final LocalTime open = LocalTime.of(8, 0);
     private static final LocalTime close = LocalTime.of(16, 0);
-    private static final LocalTime maxSession = LocalTime.of(0, 30);
-    private static final LocalTime minSession = LocalTime.of(0, 15);
+    private static final LocalTime maxSessionTime = LocalTime.of(0, 30);
+    private static final LocalTime minSessionTime = LocalTime.of(0, 10);
 
 
     public static List<String> validate(NewEventDto newEventDto) {
         List<String> result = new ArrayList<>();
 
         // validate event :
-        //validate itemName
-        //validate timeFrom and to: / duration,
-        // validate timefrom/ null, past
-        //validate timeTo null, past
 
-        Clock clock = Clock.system(ZoneId.of("Europe/Warsaw"));
-        LocalDateTime now = LocalDateTime.now(clock);
+        //validate itemName- can be null or empty
+        //validate timeFrom and to:
+        //duration- to can be before from, session can be too long session can be too short
+        // from not in open hours, not enough time for session
+        //validate timeTo null, past
+        // duration- to can be before from, session can be too long session can be too short
+        // from not in open hours, not enough time for session
 
         if (isFromTimeNull(newEventDto)) {
             result.add("From is null");
@@ -36,67 +37,83 @@ public class NewEventDtoValidator {
         //how long is event
         if (!isToTimeNull(newEventDto) && !isFromTimeNull(newEventDto)) {
             Duration durationBetween = Duration.between(newEventDto.getFromTime(), newEventDto.getToTime());
+
             if (durationBetween.isNegative()) {
                 result.add("To is before from");
             }
-            if (durationBetween.toMinutes() > maxSession.getMinute()) {
+            if (isEventTooLong(durationBetween)) {
                 result.add("Too long event");
             }
-            if (durationBetween.toMinutes() < minSession.getMinute()) {
+            if (isEventTooShort(durationBetween)) {
                 result.add("Too short event");
             }
 
             //date in the future
-            // jezeli from jest przed to,
-            if (newEventDto.getFromTime().isBefore(LocalDateTime.now())) {
+            if (isFromTimePastDate(newEventDto)) {
                 result.add("From date is from the past");
             }
-            if (newEventDto.getToTime().isBefore(LocalDateTime.now())) {
+            if (isToTimePastDate(newEventDto)) {
                 result.add("To date is from the past");
             }
 
             //date from 8 to 16
 //        zalozylam,ze najdluzsza sesja trwa 30 minut, najkrotsza 15 minut, co wplywa na graniczne wartosci godzin
 
-
-            if (newEventDto.getFromTime().getHour() < open.getHour()) {
-                result.add("From is before opening");
-            }
-            if (newEventDto.getFromTime().getHour() > close.getHour() - maxSession.getHour()) {
-                result.add("To is more than maximum session durationBetween before closing");
-            }
-            if (newEventDto.getToTime().getHour() > close.getHour()) {
-                result.add("To is after closing");
-            }
-            if (newEventDto.getToTime().getHour() < open.getHour() - minSession.getHour()) {
-                result.add("To is less than minimum session durationBetween after opening");
-            }
+//
+//            if (newEventDto.getFromTime().getHour() < open.getHour()) {
+//                result.add("From is before opening");
+//            }
+//            if (newEventDto.getToTime().getHour() > close.getHour()) {
+//                result.add("To is after closing");
+//            }
+//
+//            if (newEventDto.getFromTime().getHour() > close.getHour() - maxSession.getHour()) {
+//                result.add("To is more than maximum session maxSessionTime before closing");
+//            }
+//            if (newEventDto.getToTime().getHour() < open.getHour() - minSession.getHour()) {
+//                result.add("To is less than minimum session maxSessionTime after opening");
+//            }
         }
 
-
         //item name is null
-        if (newEventDto.getItemName() == null || newEventDto.getItemName().isEmpty()) {
+        if (isItemWithoutName(newEventDto)) {
             result.add("Item has no name");
         }
         return result;
     }
 
-    private static boolean isToTimeNull(NewEventDto newEventDto) {
+    private static boolean isEventTooLong(Duration durationBetween) {
+        return durationBetween.toMinutes() > maxSessionTime.getMinute();
+    }
 
-        if (newEventDto.getToTime() == null) {
-            return true;
-        } else return false;
+    private static boolean isEventTooShort(Duration durationBetween) {
+        return durationBetween.toMinutes() < minSessionTime.getMinute();
+    }
+
+    private static boolean isToTimePastDate(NewEventDto newEventDto) {
+        return newEventDto.getToTime().isBefore(LocalDateTime.now(Clock.systemDefaultZone()));
+    }
+
+    private static boolean isFromTimePastDate(NewEventDto newEventDto) {
+        return newEventDto.getFromTime().isBefore(LocalDateTime.now(Clock.systemDefaultZone()));
+    }
+
+    private static boolean isItemWithoutName(NewEventDto newEventDto) {
+        return newEventDto.getItemName() == null || newEventDto.getItemName().isEmpty();
+    }
+
+    private static boolean isToTimeNull(NewEventDto newEventDto) {
+        return newEventDto.getToTime() == null;
     }
 
     private static boolean isFromTimeNull(NewEventDto newEventDto) {
-
-        if (newEventDto.getFromTime() == null) {
-            return true;
-        } else return false;
+        return newEventDto.getFromTime() == null;
     }
 
+    //todo dopisac testy do TimeFrom oraz TimeTo uwzledniajace min i max dlugosc sesji
 
-    //todo  Refactoring metody validate- kazdy case wydzielic do osobnej metody private zwracajacej boolean,
-    // dodawanie do listy komunikatu, wydzielic do osobnej metody ze switchem, rozwazyc Consumera
+    //todo refactoring metody validate- podzial na validate timeFrom, validate timeTo,
+    // validate Duration Bettween,
+
 
 }
--- src/test/java/pl/sdacademy/booking/validator/NewEventDtoValidatorTest.java
+++ src/test/java/pl/sdacademy/booking/validator/NewEventDtoValidatorTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.Test;
 import pl.sdacademy.booking.model.NewEventDto;
 
 import java.time.Clock;
-import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,7 +14,7 @@ class NewEventDtoValidatorTest {
     void shouldCheckThatFromIsNull() {
         NewEventDto input = NewEventDto.builder().itemName("przyklad")
                 .fromTime(null)
-                .toTime(LocalDateTime.of(2023, 9, 19, 19, 56))
+                .toTime(LocalDateTime.now(Clock.systemDefaultZone()).plusMinutes(25L))
                 .build();
         List<String> result = NewEventDtoValidator.validate(input);
         assertThat(result).hasSize(1).contains("From is null");
@@ -26,7 +24,7 @@ class NewEventDtoValidatorTest {
     void shouldCheckThatToIsNull() {
         NewEventDto input = NewEventDto.builder().itemName("przyklad")
                 .toTime(null)
-                .fromTime(LocalDateTime.of(2023, 9, 19, 19, 56))
+                .fromTime(LocalDateTime.now(Clock.systemDefaultZone()).plusMinutes(25L))
                 .build();
         List<String> result = NewEventDtoValidator.validate(input);
         assertThat(result).hasSize(1).contains("To is null");
@@ -41,61 +39,70 @@ class NewEventDtoValidatorTest {
         List<String> result = NewEventDtoValidator.validate(input);
         assertThat(result).hasSize(2).contains("To is null");
     }
-
+    @Test
+    void shouldCheckThatToTimeIsBeforeFrom() {
+        NewEventDto input = NewEventDto.builder().itemName("przyklad")
+                .fromTime(LocalDateTime.now(Clock.systemDefaultZone()).plusMinutes(2L))
+                .toTime(LocalDateTime.now(Clock.systemDefaultZone()).minusMinutes(25L))
+                .build();
+        List<String> result = NewEventDtoValidator.validate(input);
+        assertThat(result).hasSize(3).contains("To is before from", "Too short event", "To date is from the past");
+    }
 
     @Test
-    void shouldCheckMaximumSessionDuration(){
+    void shouldCheckMaximumSessionDuration() {
         NewEventDto input = NewEventDto.builder().itemName("przyklad")
-                .fromTime(LocalDateTime.of(2023, 9, 19, 12, 00))
-                .toTime(LocalDateTime.of(2023, 9, 19, 12, 31))
+                .fromTime(LocalDateTime.now(Clock.systemDefaultZone()).plusMinutes(2L))
+                .toTime(LocalDateTime.now(Clock.systemDefaultZone()).plusMinutes(35L))
                 .build();
         List<String> result = NewEventDtoValidator.validate(input);
         assertThat(result).hasSize(1).contains("Too long event");
     }
-
     @Test
-    void shouldCheckMinimumSessionDuration(){
+    void shouldCheckMinimumSessionDuration() {
         NewEventDto input = NewEventDto.builder().itemName("przyklad")
-                .fromTime(LocalDateTime.of(2023, 9, 19, 12, 00))
-                .toTime(LocalDateTime.of(2023, 9, 19, 12, 5))
+                .fromTime(LocalDateTime.now(Clock.systemDefaultZone()).plusMinutes(2L))
+                .toTime(LocalDateTime.now(Clock.systemDefaultZone()).plusMinutes(10L))
                 .build();
         List<String> result = NewEventDtoValidator.validate(input);
         assertThat(result).hasSize(1).contains("Too short event");
     }
+
     @Test
     void shouldCheckThatFromIsPast() {
         NewEventDto input = NewEventDto.builder().itemName("przyklad")
-                .toTime(LocalDateTime.now().plusMinutes(20))
-                .fromTime(LocalDateTime.of(2022, 9, 21, 10, 30))
+                .toTime(LocalDateTime.now(Clock.systemDefaultZone()).plusMinutes(2L))
+                .fromTime(LocalDateTime.now(Clock.systemDefaultZone()).minusMinutes(15L))
                 .build();
         List<String> result = NewEventDtoValidator.validate(input);
-        assertThat(result).hasSize(2).contains("From date is from the past");
+        assertThat(result).hasSize(1).contains("From date is from the past");
     }
-    @Test
-    void shouldCheckThatToIsPast(){
 
-        NewEventDto input= NewEventDto.builder().itemName("przyklad")
-                .fromTime(LocalDateTime.now())
-                .toTime(LocalDateTime.of(2022, 9, 21, 10, 30))
-                .build();
-        List<String> result=NewEventDtoValidator.validate(input);
-        assertThat(result).hasSize(3).contains("To date is from the past");
-    }
     @Test
-    void shouldCheckThatToAndFromIsPast(){
-        NewEventDto input= NewEventDto.builder().itemName("przyklad")
-                .fromTime(LocalDateTime.now().minusSeconds(1L))
-                .toTime(LocalDateTime.now().minusSeconds(1L))
+    void shouldCheckThatToIsPast() {
+        NewEventDto input = NewEventDto.builder().itemName("przyklad")
+                .fromTime(LocalDateTime.now(Clock.systemDefaultZone()))
+                .toTime(LocalDateTime.now(Clock.systemDefaultZone()).minusMinutes(2L))
                 .build();
-        List<String> result=NewEventDtoValidator.validate(input);
-        assertThat(result).hasSize(2).contains("To date is from the past");
+        List<String> result = NewEventDtoValidator.validate(input);
+        assertThat(result).hasSize(3).contains("To is before from", "Too short event", "To date is from the past");
+    }
+
+    @Test
+    void shouldCheckThatToAndFromIsPast() {
+        NewEventDto input = NewEventDto.builder().itemName("przyklad")
+                .fromTime(LocalDateTime.now(Clock.systemDefaultZone()).minusMinutes(2L))
+                .toTime(LocalDateTime.now(Clock.systemDefaultZone()).minusMinutes(2L))
+                .build();
+        List<String> result = NewEventDtoValidator.validate(input);
+        assertThat(result).hasSize(3).contains("Too short event", "From date is from the past", "To date is from the past");
     }
 
     @Test
     void shouldCheckThatItemNameIsEmpty() {
         NewEventDto input = NewEventDto.builder().itemName("")
-                .fromTime(LocalDateTime.of(2023, 9, 19, 12, 00))
-                .toTime(LocalDateTime.of(2023, 9, 19, 12, 25))
+                .fromTime(LocalDateTime.now().plusMinutes(2L))
+                .toTime(LocalDateTime.now().plusMinutes(20L))
                 .build();
         List<String> result = NewEventDtoValidator.validate(input);
         assertThat(result).hasSize(1).contains("Item has no name");
@@ -104,10 +111,35 @@ class NewEventDtoValidatorTest {
     @Test
     void shouldCheckThatItemNameIsNull() {
         NewEventDto input = NewEventDto.builder().itemName(null)
-                .fromTime(LocalDateTime.of(2023, 9, 19, 12, 00))
-                .toTime(LocalDateTime.of(2023, 9, 19, 12, 25))
+                .fromTime(LocalDateTime.now().plusMinutes(2L))
+                .toTime(LocalDateTime.now().plusMinutes(20L))
                 .build();
         List<String> result = NewEventDtoValidator.validate(input);
         assertThat(result).hasSize(1).contains("Item has no name");
     }
+
+    /**
+
+
+     *     @Test
+     *     void shouldCheckThatFromIsBefore8() {
+     *         NewEventDto input= NewEventDto.builder().itemName("przyklad")
+     *                 .toTime(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(8,9)))
+     *                 .fromTime(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(7,59)))
+     *                 .build();
+     *         List<String> result=NewEventDtoValidator.validate(input);
+     *         assertThat(result).hasSize(1).contains("Time out of working hours");
+     *     }
+     *     @Test
+     *     void shouldCheckThatToIsAfter16() {
+     *         NewEventDto input= NewEventDto.builder().itemName("przyklad")
+     *                 .toTime(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(16,9)))
+     *                 .fromTime(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(15,50)))
+     *                 .build();
+     *         List<String> result=NewEventDtoValidator.validate(input);
+     *         assertThat(result).hasSize(1).contains("Time out of working hours");
+     *     }
+     *
+     */
+
 }
